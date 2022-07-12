23410a02697da17f3dd4e78df9f53d196cb8372c
KGouthamReddy <kasireddygouthamreddy74135@gmail.com>
2022-07-12 17:52:45 +0530

    my java helloworld project files

---
+++ helloworld/src/helloworld/myclass.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class myclass {
+	public static void main(String args[]) {
+		System.out.println("Helweorijfvgjoshgoi yjetoiehgoifnvidhfe aoigfhreshgsn voi rgh roij fofsivhno");
+		System.out.println(".....");
+		System.out.println("Helwasrhfgruew hvr asfghwahfowaj fuhfowahfjoiwrhjesf");
+	}
+
+}
