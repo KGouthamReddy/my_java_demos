d9a64064e68e72c46b971fd801a321eb13b11238
KGouthamReddy <kasireddygouthamreddy74135@gmail.com>
2022-07-14 12:59:01 +0530

    problem solving in class

---
+++ project_test/src/project_test/parsing.java
@@ -0,0 +1,19 @@
+package project_test;
+
+public class parsing {
+
+public static void main(String args[]) {
+		
+		String num = "12";
+		
+		System.out.println(num);
+		
+		
+		int num1 = Integer.parseInt(num);
+		
+		System.out.println(num1);
+		
+		
+	}
+
+}
